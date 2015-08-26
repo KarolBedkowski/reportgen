eee1c02afb2e37372dde971423220be09f117803
Karol Będkowski <karol.bedkowski@gmail.com>
2015-08-26 22:16:32 +0200

    uupdate run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DB_JAR=./lib/sqlite-jdbc-3.7.2.jar
-java -classpath ./raportgen-0.0.1-SNAPSHOT.jar:$DB_JAR prv.k.reportgen.App test.yml
+java -classpath ./raportgen-0.0.2.jar:$DB_JAR prv.k.reportgen.App test-db.toml test.toml
